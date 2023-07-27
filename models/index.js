14b7d82593f5f60a2c2ca1ae019b62b679294cee
Michael O'Sullivan <michaelhallosullivan@gmail.com>
2023-07-27 11:09:48 -0700

    added template code from nosql mini-project

---
+++ models/index.js
@@ -0,0 +1,8 @@
+const User = require('./User');
+const Reaction = require('./Reaction');
+const Thought = require('./Thought');
+//const Friend = require('./Friend');
+
+module.exports = { User, Reaction, Thought };
+
+//Friends belong to users?
